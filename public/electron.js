b7aa5ac47bf66b035cbb3597b042c11c29309d4c
Richard Pecha <pecharichard26@gmail.com>
2021-03-12 23:31:11 +0100

    update main file

--- public/electron.js
+++ public/electron.js
@@ -5,15 +5,7 @@ const { autoUpdater } = require("electron-updater")
 const log = require('electron-log');
 const isDev = require("electron-is-dev");
 
-try {
-  require('electron-reloader')(module, {
-    watchRenderer: true,
-  });
-} catch (_) {
-  console.log('Error');
-}
-
-function createWindow() {
+const createWindow =() =>{
   autoUpdater.logger = log;
   autoUpdater.logger.transports.file.level = 'info';
   autoUpdater.autoDownload=false;
@@ -35,8 +27,6 @@ function createWindow() {
     height: 480,
   });
 
-  //window.maximize();
-  
   window.loadURL(
     isDev
       ? "http://localhost:3000"
@@ -117,7 +107,7 @@ function createWindow() {
 
 }
 
-function runProxyServer() {
+const runProxyServer = () => {
   let pathLevel = '../';
   if (process.argv[2] !== '--dev') {
     pathLevel += '../../';
@@ -132,12 +122,28 @@ function runProxyServer() {
       cwd: path.join(__dirname, pathLevel + 'bin/proxy'),
     },
     (err, data) => {
-      //throw err;
-      /*console.log(err);
-        console.log(data.toString());*/
+      logInfo(err);
     }
   );
 }
 
 
-app.on('ready', createWindow);
+app.on('ready', ()=>  {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+const logInfo =(text) =>{
+  log.info(text);
+}
